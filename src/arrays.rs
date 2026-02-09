62eefbd81d707ef5eb9f4227d4ea05fccc5349e3
niiMarrion <jephtahaddison@gmail.com>
2026-02-09 15:32:38 +0000

    learning rust

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,4 +1,3 @@
 pub fn set_array([x, y, z]: [i32; 3]) {
-    println!("The value of x is: {}, y is: {}, z is: {}", x, y, z);
-    
+    println!("The value of x is: {}, y is: {}, z is: {}", x, y, z);  
 }
--- src/tuples.rs
+++ src/tuples.rs
@@ -2,5 +2,4 @@ pub fn set_tuple((x, y, z): (i32, f64, char)) {
     
     println!("The value of x is: {}, y is: {}, z is: {}", x, y, z);
 
-    
 }
